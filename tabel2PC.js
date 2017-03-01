75d73b741e7b226fe81ef004b560f74ac31e2474
William <williamaggerhein@gmail.com>
2017-03-01 14:18:10 +0100

    Update tabel2PC.js
    Update for the new emat.dk versions

--- tabel2PC.js
+++ tabel2PC.js
@@ -1,1 +1,1 @@
-javascript:for(count = 0; count < 2; count++){document.getElementById("Textbox_Svar").value=HuskFacit; STast(-2);}document.getElementById("Textbox_Svar").value=HuskFacit;
+javascript:for(count = 0; count < 2; count++){document.getElementById("Textbox_Svar").value=HuskFacit2; STast(-2);}document.getElementById("Textbox_Svar").value=HuskFacit2;
